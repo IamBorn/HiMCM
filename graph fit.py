7c913e9c75a8d56e8ffc773fa1b3325c5c210f5a
IamBorn <piyoungkoon@gmail.com>
2022-11-12 11:00:24 +0700

    add linear curve fit

--- graph fit.py
+++ graph fit.py
@@ -4,6 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 from scipy.optimize import curve_fit
+from scipy import stats
+
+def objective_linear(x, a, b):
+    return a*x + b
 
 def objective_quadratic(x,a,b,c):
     return a*x**2 + b*x + c
@@ -17,14 +21,27 @@ data = data.rename(columns={"PPM": "carbon level"})
 
 print(data.to_string())
 
-fit_paramsQ, covariances = curve_fit(objective_quadratic, data["Year"], data["carbon level"])
-a, b, c = fit_paramsQ
-print('Parameter values: ')
-print('y = %.5f * x^2 + %.5f * x + %.5f' % (a, b, c))
-y_fit = objective_quadratic(data["Year"],*fit_paramsQ)
+def quadratic_fit(data):
+    fit_paramsQ, covariances = curve_fit(objective_quadratic, data["Year"], data["carbon level"])
+    a, b, c = fit_paramsQ
+    y_fit = objective_quadratic(data["Year"],*fit_paramsQ)
+    plt.plot(data["Year"], y_fit, '--', color='red', label='q fit')
+    slope, intercept, r_value, p_value, std_err = stats.linregress(data["Year"],y_fit)
+    return r_value
+
+
+def linear_fit(data):
+    fit_paramsl, covariances = curve_fit(objective_linear, data["Year"], data["carbon level"])
+    a, b = fit_paramsl
+    y_fit = objective_linear(data["Year"],*fit_paramsl)
+    plt.plot(data["Year"], y_fit, '--', color='red', label='linear fit')
+    slope, intercept, r_value, p_value, std_err = stats.linregress(data["Year"],y_fit)
+    return r_value
+
 
 plt.xlabel('Year')
 plt.ylabel('Carbon dioxide level[PPM]')
-plt.plot(data["Year"], y_fit, '--', color='red')
+
 plt.plot(data["Year"], data["carbon level"])
-plt.savefig('carbon dioxide level quadratic fit.png')
+print(linear_fit(data))
+plt.show()
